0e1398a7e38890580b032c5e70232400218b4e15
KseniyaKirava <kirova.ksu@gmail.com>
2018-10-09 03:27:38 +0300

    validator+

---
+++ src/main/java/by/htp/kirova/task2/java/service/validation/ValidationPattern.java
@@ -0,0 +1,52 @@
+package by.htp.kirova.task2.java.service.validation;
+
+import java.util.regex.Pattern;
+
+/**
+ * Contains precompiled patterns for validating user's data.
+ *
+ * @author Kseniya Kirava
+ * @since Sep 24, 2018
+ */
+
+enum ValidationPattern {
+
+    /**
+     * User's full name pattern.
+     */
+    USERNAME("[[A-Za-z._-]+]{2,15}"),
+
+    /**
+     * User's password pattern.
+     */
+    PASSWORD("[\\w]{6,15}"),
+
+    /**
+     * User's full name pattern.
+     */
+    NAME("[[A-Za-zА-Яа-яЁё]+]{2,15}"),
+
+    /**
+     * E-mail pattern.
+     */
+    EMAIL("([\\w\\.\\w]+)@(\\w+\\.)([a-z]{2,4})"),
+
+    /**
+     * Date pattern.
+     */
+    DATE("([0-9]{2})([\\.])([0-9]{2})([\\.])([0-9]{4})");
+
+
+
+    private Pattern pattern;
+
+    ValidationPattern(String regEx){
+        this.pattern = Pattern.compile(regEx);
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+}
+
+
---
+++ src/main/java/by/htp/kirova/task2/java/service/validation/Validator.java
@@ -0,0 +1,92 @@
+package by.htp.kirova.task2.java.service.validation;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * Validates data such as e-mail, name and password, by checking it against
+ * predefined patterns. Utility class, therefore final as it's not designed for
+ * instantiation and extension.
+ *
+ * @author Kseniya Kirava
+ * @since Sep 24, 2018
+ */
+
+public final class Validator {
+
+
+    /**
+     * Instance of {@code org.apache.log4j.Logger} is used for logging.
+     */
+    private static final Logger LOGGER = Logger.getLogger(Validator.class);
+
+    /**
+     * Default private constructor as this class entirely utility.
+     */
+    private Validator() {
+    }
+
+    /**
+     * Checks the username against user's username pattern.
+     *
+     * @param username user's username.
+     * @return {@code true} in case of success and false otherwise.
+     */
+    public static boolean checkUsername(String username) {
+        boolean isValid = username != null && ValidationPattern.USERNAME.getPattern().matcher(username).matches();
+        LOGGER.info("Username is valid: " + isValid);
+        return isValid;
+    }
+
+    /**
+     * Checks the first name/last name/middle name against user's name pattern.
+     *
+     * @param name user's first name/last name/middle name.
+     * @return {@code true} in case of success and false otherwise.
+     */
+    public static boolean checkName(String name) {
+        boolean isValid = name != null && ValidationPattern.NAME.getPattern().matcher(name).matches();
+        LOGGER.info("User name is valid: " + isValid);
+        return isValid;
+    }
+
+    /**
+     * Checks the e-mail against user's e-mail pattern.
+     *
+     * @param email user's e-mail.
+     * @return {@code true} in case of success and false otherwise.
+     */
+    public static boolean checkEmail(String email) {
+        boolean isValid = email != null && ValidationPattern.EMAIL.getPattern().matcher(email).matches();
+        LOGGER.info("User e-mail is valid: " + isValid);
+        return isValid;
+    }
+
+    /**
+     * Checks the password against user's password pattern.
+     *
+     * @param password user's password.
+     * @return {@code true} in case of success and false otherwise.
+     */
+    public static boolean checkPassword(String password) {
+        boolean isValid = password != null && ValidationPattern.PASSWORD.getPattern().matcher(password).matches();
+        LOGGER.info("User password is valid: " + isValid);
+        return isValid;
+    }
+
+    /**
+     * Checks the checkin/checkout/registration date against date pattern.
+     *
+     * @param date checkin/checkout/registration date
+     * @return {@code true} in case of success and false otherwise.
+     */
+    public static boolean checkDate(String date) {
+        boolean isValid = date!=null && ValidationPattern.DATE.getPattern().matcher(date).matches();
+        LOGGER.info("Date is valid: " + isValid);
+        return isValid;
+    }
+
+}
+
+
+
